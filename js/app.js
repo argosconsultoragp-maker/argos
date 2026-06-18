2bd4241cd9685739076a62725b4ddd9dca01f1e3
argosconsultoragp-maker <argosconsultoragp@gmail.com>
2026-06-18 11:58:33 -0300

    Add particle animation configuration to app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,44 @@ window.addEventListener('scroll', () => {
   const sections = document.querySelectorAll('section');
 
 });
+tsParticles.load("particles", {
+
+    background: {
+        color: {
+            value: "transparent"
+        }
+    },
+
+    particles: {
+
+        number: {
+            value: 60
+        },
+
+        color: {
+            value: "#ffc107"
+        },
+
+        links: {
+            enable: true,
+            color: "#ffc107",
+            distance: 150,
+            opacity: 0.3
+        },
+
+        move: {
+            enable: true,
+            speed: 1
+        },
+
+        opacity: {
+            value: 0.5
+        },
+
+        size: {
+            value: 3
+        }
+
+    }
+
+});
